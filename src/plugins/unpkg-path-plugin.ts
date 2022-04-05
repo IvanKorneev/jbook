0cbace7a294e6a0ba9356e88d0df81ff44dcbdfb
IvanKorneev <ivan.kornieiev@gmail.com>
2022-04-05 11:37:16 +0300

    add localForage and fix error Ts

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,12 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from "axios";
+import localForage from "localforage";
+
+
+const fileCache = localForage.createInstance({
+    name: 'filecache'
+});
+
 
 export const unpkgPathPlugin = () => {
     return {
@@ -28,18 +35,25 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         contents: ` 
-                        const react = require('react')
-                        const reactDOM = require('react-dom');
-                        console.log(react,reactDOM)
+                      import React,{useState} from 'react-select'
+                      console.log(React,useState)
                      `,
                     };
                 }
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+                if (cachedResult) {
+                    return cachedResult;
+                }
+
                 const {data, request} = await axios.get(args.path);
-                return {
+                const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
                     contents: data,
                     resolveDir: new URL('./', request.responseURL).pathname,
-                }
+                };
+                await fileCache.setItem(args.path, result);
+                return result
             });
         },
     };
